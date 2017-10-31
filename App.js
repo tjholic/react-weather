9f001222f2110b02172c023a1ed98d9b22d77dbb
TaejinKwon <taejinkwon87@gmail.com>
2017-11-01 02:21:04 +0900

    Get Weather data.

--- App.js
+++ App.js
@@ -1,18 +1,52 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Weather from './Weather'
+import React from "react";
+import { StyleSheet, Text, View, StatusBar } from "react-native";
+import Weather from "./Weather";
+
+const API_KEY_OPEN_WEATHER_MAP = "your_key";
 
 export default class App extends React.Component {
   state = {
-    isLoaded: true
+    isLoaded: false,
+    err: null,
+    temperature: null,
+    name: null
+  };
+  componentWillMount() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        getWeather(position.coords.latitude, position.coords.longitude);
+      },
+      error => {
+        err: error;
+      }
+    );
   }
+
+  getWeather = (lat, lon) => {
+    fetch(
+      `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY_OPEN_WEATHER_MAP}`
+    )
+      .then(response => response.json())
+      .then(json => {
+        console.log(json);
+        this.setState({
+          temperature: json.main.temp,
+          name: json.weather[0].main,
+          isLoaded: true
+        });
+      });
+  };
   render() {
-    const { isLoaded } = this.state;
+    const { isLoaded, err } = this.state;
     return (
       <View style={styles.container}>
-        {isLoaded ? null : (
+        <StatusBar hidden={true} />
+        {isLoaded ? (
+          <Weather />
+        ) : (
           <View style={styles.loading}>
             <Text style={styles.loadingText}>Getting the fucking weather</Text>
+            {err ? <Text style={styles.errorText}>{err}</Text> : null}
           </View>
         )}
       </View>
@@ -23,16 +57,22 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff', 
+    backgroundColor: "#fff"
+  },
+  errorText: {
+    fontSize: 20,
+    color: "red",
+    backgroundColor: "transparent",
+    marginBottom: 40
   },
   loading: {
-    flex:1,
-    backgroundColor:'#FDF6AA',
-    justifyContent: 'flex-end',
+    flex: 1,
+    backgroundColor: "#FDF6AA",
+    justifyContent: "flex-end",
     paddingLeft: 30
   },
   loadingText: {
-    fontSize:38,
-    marginBottom: 100
+    fontSize: 38,
+    marginBottom: 24
   }
 });
--- Weather.js
+++ Weather.js
@@ -1,20 +1,58 @@
-import React, { Compoent } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { LinearGradient } from 'expo';
+import React, { Compoent } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { LinearGradient } from "expo";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-
-export default class Weather extends Component {
-    render() {
-        return (
-            <LinearGradient colors={["#00C6FB", "#005BEA"]} style={styles.container}>
-
-            </LinearGradient>
-        )
-    }
+export default class Weather extends React.Component {
+  render() {
+    return (
+      <LinearGradient colors={["#00C6FB", "#005BEA"]} style={styles.container}>
+        <View style={styles.upper}>
+          <MaterialCommunityIcons color="white" size={144} name="weather-rainy"/>
+          <Text style={styles.temp}>35°C</Text>
+        </View>
+        <View style={styles.lower}>
+          <Text style={styles.title}>Raining like a MF</Text>
+          <Text style={styles.subTitle}>For more info look outside.</Text>
+        </View>
+      </LinearGradient>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1
-    }
-})
+  container: {
+    flex: 1
+  },
+  temp: {
+    fontSize: 48,
+    backgroundColor: "transparent",
+    color: "white",
+    marginTop: 10
+  },
+  upper: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "transparent",
+  },
+  lower: {
+    flex: 1,
+    alignItems: "flex-start",
+    justifyContent: "flex-end",
+    paddingLeft: 25
+  },
+  title: {
+    fontSize: 38,
+    backgroundColor: "transparent",
+    color: "white",
+    marginBottom: 10,
+    fontWeight: "300"
+  },
+  subTitle: {
+    fontSize: 24,
+    backgroundColor: "transparent",
+    color: "white",
+    marginBottom: 24
+  }
+});
